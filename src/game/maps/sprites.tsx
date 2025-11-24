e52734301608a8c15603bdaf134f6339248fb7df
Cassidy Williams <cassidoo@users.noreply.github.com>
2025-11-24 11:52:08 -0600

    Add maps for every level

---
+++ src/game/maps/sprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="sprites" tilewidth="16" tileheight="16" tilecount="275" columns="25">
+ <image source="sprites.png" trans="ff00ff" width="400" height="176"/>
+</tileset>
